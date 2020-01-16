41c4ee1d3ffc16e5567fee00a1f45be42acae015
Pratik <Pratikpatre2019@gmail.com>
2020-01-16 16:33:24 +0530

    insialled  the cra and added the file sructure

---
+++ src/pages/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The main component</h1>
+      </div>
+    );
+  }
+}
+export default Main;
